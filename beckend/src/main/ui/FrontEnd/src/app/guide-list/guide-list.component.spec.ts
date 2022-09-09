22badefde414a90c15ec19b0f6ed1cd9fc5b195f
Kamil <Kamil.Urbanik@o2.pl>
2022-09-09 16:53:57 +0200

    #0008 - add guides list in angular

---
+++ beckend/src/main/ui/FrontEnd/src/app/guide-list/guide-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GuideListComponent } from './guide-list.component';
+
+describe('GuideListComponent', () => {
+  let component: GuideListComponent;
+  let fixture: ComponentFixture<GuideListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GuideListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GuideListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ beckend/src/main/ui/FrontEnd/src/app/guide.spec.ts
@@ -0,0 +1,7 @@
+import { Guide } from './guide';
+
+describe('Guide', () => {
+  it('should create an instance', () => {
+    expect(new Guide()).toBeTruthy();
+  });
+});
